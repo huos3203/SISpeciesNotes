4d57febe2dd8facfd1460f532d3777702cade68d
pengyucheng <pengyucheng@huoshug.local>
2016-03-30 11:44:22 +0800

    整理目录，创建转场动画

---
+++ SISpeciesNotes/animatedTransition/OverlayAnimationController.swift
@@ -0,0 +1,13 @@
+//
+//  OverlayAnimationController.swift
+//  SISpeciesNotes
+//
+//  Created by pengyucheng on 16/3/30.
+//  Copyright © 2016年 益行人. All rights reserved.
+//
+
+import UIKit
+
+class OverlayAnimationController: NSObject {
+
+}
