13e99e49eb7fac41a5b815ae9aed9fcb3850a4bc
huos3203 <724987481@qq.com>
2016-04-12 01:12:57 +0800

    练习xctest xcode7相关功能，@testable import module问题，引入模块

---
+++ SISpeciesNotes/Controller/PercentageCalculatorViewController.swift
@@ -0,0 +1,92 @@
+//
+//  PercentageCalculatorViewController.swift
+//  SISpeciesNotes
+//
+//  Created by huoshuguang on 16/4/11.
+//  Copyright © 2016年 益行人. All rights reserved.
+//
+
+import UIKit
+
+class PercentageCalculatorViewController: UIViewController {
+
+    @IBOutlet weak var ibNumberSlider: UISlider!
+    @IBOutlet weak var ibNumberLabel: UILabel!
+    
+    @IBOutlet weak var ibPercentageSlider: UISlider!
+    @IBOutlet weak var ibPercentageLabel: UILabel!
+    
+    @IBOutlet weak var ibResultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+//    拖动事件
+    
+    @IBAction func ibaNumberSliderChangeValueAction(sender: UISlider) {
+        
+//        更新numberLabel 和 ResultLabel
+//        resultLabel = number * percentage * 0.01
+        let result = updateResult((Int(sender.value),Int(self.ibPercentageSlider.value)))
+        self.ibNumberLabel.text = "\(result.0)"
+        self.ibResultLabel.text = "\(result.1)"
+        
+        
+        var sender = Int(sender.value)
+        let result2 = updateResult(&sender, num: Int(self.ibPercentageSlider.value))
+        self.ibNumberLabel.text = "\(sender)"
+        self.ibResultLabel.text = "\(result2)"
+        
+    }
+    
+    @IBAction func ibaPercentageSliderAction(sender: UISlider) {
+        
+//        更新 percentageLabel 和 resultLabel
+        
+//        resultLabel = number * percentage * 0.01
+        let result = updateResult((Int(sender.value),Int(self.ibNumberSlider.value)))
+        
+        self.ibPercentageLabel.text = "\(result.0)"
+        
+        self.ibResultLabel.text = "\(result.1)"
+    }
+    
+//    通过inout形参
+    func updateResult(inout sender:Int,num:Int)->Int
+    {
+        let result = sender * num / 100
+        sender += 1
+        return result
+    }
+    
+    
+    
+    func updateResult(num:(Int,Int)) -> (Int,Int)
+    {
+        let result = num.0 * num.1 / 100
+        return (num.0,Int(result))
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SISpeciesNotesTests/PercentageCalculator.swift
@@ -0,0 +1,70 @@
+//
+//  PercentageCalculator.swift
+//  SISpeciesNotes
+//
+//  Created by huoshuguang on 16/4/11.
+//  Copyright © 2016年 益行人. All rights reserved.
+//
+
+//[原文](http://www.jianshu.com/p/b0525df8e68f)
+//
+import XCTest
+@testable import SISpeciesNotes
+//http://www.cocoachina.com/ios/20151125/14415.html
+//http://stackoverflow.com/questions/32008403/no-such-module-when-using-testable-in-xcode-unit-tests
+//http://www.tuicool.com/articles/rMzMjaa
+
+class PercentageCalculator: XCTestCase {
+    var vc:PercentageCalculatorViewController!
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    var p = PercentageCalculatorViewController()
+        
+        let storyboard = UIStoryboard(name:"Main",bundle: NSBundle.mainBundle())
+        vc = storyboard.instantiateInitialViewController() as! PercentageCalculatorViewController
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    
+    func testPercentageCalculator()
+    {
+        //测试
+       var sender = 50
+       let result = vc.updateResult(&sender, num: 50)
+       XCTAssert(result == 25)
+    }
+    
+    //    我已经在storyboard中已经建好了这些lables，并且在视图加载的时候就已经进行了初始化任务，但是在单元测试部分loadView()方法永远都不会触发因此这些值都是空值。一个可能的解决方法是我们自己调用vc.loadView()，但是这种做法官方文档里面并不提倡，因为这种做法可能导致内存泄漏（当视图进行了多次加载的时候）。
+    
+//    正确的解决方法是，访问vc的view属性。该做法会触发所有需要的方法，不仅仅只有loadView()。
+    
+    func testLabelValuesShowedProperly() {
+        
+//        注意到我们使用下划线_来代表一个常量名。这是因为我们并不真正需要这个常量也永远不会使用这个常量。它只是用来告诉编译器”假装访问并触发视图的所有方法。“
+        let _ = vc.view
+        
+//        vc.upda (Float(80.0), Float(50.0), Float(40.0))
+//        
+//        // The labels should now display 80, 50 and 40
+//        XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
+//        XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+//        XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
+    }
+}
