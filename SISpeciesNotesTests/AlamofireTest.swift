67f6fa9adfc585abab4bce103780a2c7c947acd5
pengyucheng <pengyucheng@huoshug.local>
2016-04-18 18:44:43 +0800

    新建Alamofire

---
+++ SISpeciesNotesTests/AlamofireTest.swift
@@ -0,0 +1,24 @@
+//
+//  AlamofireTest.swift
+//  SISpeciesNotes
+//
+//  Created by pengyucheng on 16/4/18.
+//  Copyright © 2016年 布衣男儿. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
