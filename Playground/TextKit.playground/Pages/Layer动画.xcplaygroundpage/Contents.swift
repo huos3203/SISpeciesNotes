1ad8c7cb2ec411177391a5876923ef2587331bcd
pengyucheng <pengyucheng@huoshug.local>
2016-05-06 18:31:13 +0800

    图层动画

---
+++ Playground/TextKit.playground/Pages/Layer动画.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+import UIKit
+
+//http://www.cocoachina.com/ios/20141022/10005.html
+class drawLayerViewController: UIViewController {
+    //
+    
+    override func viewDidLoad() {
+        //
+        let layer = DrawLayer.DrawLayer(view.bounds)
+    }
+}
+
+//: [Next](@next)
