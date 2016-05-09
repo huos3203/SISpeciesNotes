32f3e3fb008ca98722a3bfee936158cf7ea020d2
pengyucheng <pengyucheng@huoshug.local>
2016-05-09 19:24:53 +0800

    发现问题：//swift方法映射到javaScript中的方法无效

--- Playground/Java​Script​Core.playground/Pages/实战.xcplaygroundpage/Contents.swift
+++ Playground/Java​Script​Core.playground/Pages/实战.xcplaygroundpage/Contents.swift
@@ -4,6 +4,7 @@ import Foundation
 import JavaScriptCore
 import UIKit
 
+let context = JSContext()
 //目的：定义一个 Person 模型符合 JSExport 子协议 PersonJSExports 的例子，然后使用 JavaScript 从 JSON 文件中创建并填充实例。都有一个完整的 JVM 在那儿了，谁还需要 NSJSONSerialization？
 
 // Custom protocol must be declared with `@objc`
@@ -16,7 +17,7 @@ import UIKit
     func getFullName() -> String
     
     /// create and return a new Person instance with `firstName` and `lastName`
-    static func createWithFirstName() -> Person
+    static func createWithFirstName(firstName: String, lastName: String) -> Person
 }
 
 // Custom class must inherit from `NSObject`
@@ -36,8 +37,8 @@ import UIKit
     //由于 JavaScriptCore 没有初始化，所以 create... 类方法是必要的，我们不能像原生的 JavaScript 类型那样简单地用 var person = new Person()。
     //这段代码将创建新的 Person 实例。
     //javaScriptCore 转换的 Objective-C / Swift 方法名是 JavaScript 兼容的。由于 JavaScript 没有参数 名称，任何外部参数名称都会被转换为驼峰形式并且附加到函数名后。在这个例子中，Objective-C 的方法 createWithFirstName:lastName: 变成了在JavaScript中的 createWithFirstNameLastName()
-    static func createWithFirstName() -> Person {
-        return Person(firstName: "dddd1", lastName: "ddddd2")
+    class func createWithFirstName(firstName: String, lastName: String) -> Person {
+        return Person(firstName: firstName, lastName: lastName)
     }
     
     func getFullName() -> String {
@@ -45,13 +46,15 @@ import UIKit
     }
 }
 
-//JSContext 配置
+
 
 //之前，我们可以用我们已经创建的 Person 类，我们需要将其导出到 JavaScript 环境。我们也将借此导入 Mustache JS library，我们将应用模板到我们的 Person 对象。
 
-let context = JSContext()
-// export Person class
+// #### JSContext 配置
+// export Person class   //context[@"Person"] = [Person class];
+//[对象下标索引](http://nshipster.cn/object-subscripting/ )
 context.setObject(Person.self, forKeyedSubscript: "Person")
+
 
 // load Mustache.js
 if let mustacheJSString = try? NSString(contentsOfURL:[#FileReference(fileReferenceLiteral: "Mustache.js")#], encoding:NSUTF8StringEncoding) {
@@ -63,8 +66,9 @@ if let peopleJSON = try? NSString(contentsOfURL:[#FileReference(fileReferenceLit
     print([peopleJSON])
     // get load function
     let load = context.objectForKeyedSubscript("loadPeopleFromJSON")
+    
     let peoples = context.objectForKeyedSubscript("loadPeople")
-    let result3 = load.callWithArguments([peopleJSON])
+    let ddd = load.callWithArguments([peopleJSON])
     let result = peoples.callWithArguments(["dddd"])
     print("\(result.toArray)")
     // call with JSON and convert to an array of `Person`
--- Playground/Java​Script​Core.playground/Pages/实战.xcplaygroundpage/Resources/Mustache.js
+++ Playground/Java​Script​Core.playground/Pages/实战.xcplaygroundpage/Resources/Mustache.js
@@ -3,8 +3,8 @@ var loadPeopleFromJSON = function(jsonString) {
     var data = JSON.parse(jsonString);
     var people = [];
     for (i = 0; i < data.length; i++) {
-        
-        var person = Person.createWithFirstName();
+        //初始化方法无效.....
+        var person = Person.createWithFirstNameLastName(data[i].first, data[i].last);
         person.birthYear = data[i].year;
         
         people.push(data[i].year);
