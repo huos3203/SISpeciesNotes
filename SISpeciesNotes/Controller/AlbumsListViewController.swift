3594221a6fe36a2b90c963abaae5e1f1188bd948
pengyucheng <pengyucheng@huoshug.local>
2016-05-10 17:29:01 +0800

    cixi

---
+++ SISpeciesNotes/Controller/AlbumsListViewController.swift
@@ -0,0 +1,159 @@
+//
+//  AlbumsListViewController.swift
+//  SISpeciesNotes
+//
+//  Created by huoshuguang on 16/4/19.
+//  Copyright © 2016年 布衣男儿. All rights reserved.
+//
+
+import UIKit
+
+
+/// 显示所有专辑列表
+public class AlbumsListViewController: UIViewController {
+    
+    //存储变量
+    private var tableView = UITableView()
+    private var scroller = HorizontalScroller()
+    private var currentIndex = 0
+    //系列数组
+    private var albums = [Album]()
+    
+    //详情页内容
+    private var albumDetail:(title:String,value:String)?
+    
+    //周期方法
+    override public func viewDidLoad() {
+        //获取数据
+        albums = LibraryAPI.shareInstance.getAlbums()
+        initScroller()
+        initTableView()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AlbumsListViewController.saveCurrentImageIndex), name: UIApplicationDidEnterBackgroundNotification, object: nil)
+     }
+    
+    
+    //MARK: - 初始化子控件
+    
+    //轮播图
+    func initScroller(){
+        //轮播图
+        scroller.scrollerDataSource = self
+        scroller.scrollerDelegate = self
+        view.addSubview(scroller)
+        scroller.snp_makeConstraints { (make) in
+            //
+            make.height.equalTo(200)
+            make.top.equalTo(self.snp_topLayoutGuideBottom).offset(20)
+            make.left.right.equalTo(view).inset(8)
+        }
+        loadCurrentImageIndex()
+        scroller.initScrollView(200, imgPadding: 10)
+    }
+    
+    //tableView
+    func initTableView(){
+        //TableView协议
+        tableView.delegate = self
+        tableView.dataSource = self
+        //注册cell identifier
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        tableView.snp_makeConstraints { (make) in
+            //
+            make.left.right.bottom.equalTo(view).inset(8)
+            make.top.equalTo(scroller.snp_bottom).offset(10)
+        }
+
+    }
+    
+    //MARK: 备忘录：加载/持久化轮播图当前状态
+    func saveCurrentImageIndex() {
+        //
+        NSUserDefaults.standardUserDefaults().setInteger(currentIndex, forKey: "CurrentImageIndex")
+    }
+    
+    func loadCurrentImageIndex() {
+        //
+        currentIndex = NSUserDefaults.standardUserDefaults().integerForKey("CurrentImageIndex")
+    }
+}
+
+
+
+// MARK: - 修饰模式，使用扩展来实现表格控制器相关的委托方法
+extension AlbumsListViewController:UITableViewDelegate{
+    //代理tableView点击某个单元格的事件
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //提示点击了某个单元格
+        let alertView = UIAlertController.init(title: "代理事件", message: "点击对象是：\(albums[indexPath.row].artist)", preferredStyle: .Alert)
+        let cancelAction = UIAlertAction.init(title: "取消", style: .Cancel, handler: nil)
+        let okAction = UIAlertAction.init(title: "确定", style: .Destructive) { _ in
+            //默认消失...
+            print("点击OK")
+        }
+
+        alertView.addAction(cancelAction)
+        alertView.addAction(okAction)
+        self.presentViewController(alertView, animated: true, completion: nil)
+    }
+}
+extension AlbumsListViewController:UITableViewDataSource{
+    
+    //告诉tableView在这个类的表格中，每一行单元格的显示样式和内容
+     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //样式
+        //通过Identifier来复用单元格
+        var cellView = tableView.dequeueReusableCellWithIdentifier("cell")
+        if cellView != nil {
+            //新建cell
+            cellView = UITableViewCell.init(style: .Value1, reuseIdentifier: "cell")
+        }
+        
+        cellView?.textLabel?.text = albums[indexPath.row].title
+        cellView?.detailTextLabel?.text = albums[indexPath.row].artist
+        
+        return cellView!
+    }
+    
+    //告诉tableView每个单元中要显示cell的个数
+     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albums.count
+    }
+
+}
+
+// MARK: - 修饰模式，使用扩展来实现scroller相关委托
+extension AlbumsListViewController:HorizontalScrollerDataSource{
+    
+//    public func pageNumOfScroller() -> Int {
+//        return albums.count
+//    }
+    public var currentImageIndex: Int{
+        get{
+            return self.currentIndex
+        }
+        set{
+            self.currentIndex = newValue
+        }
+    }
+    
+    public var pageNumOfScroller: Int{
+        return albums.count
+    }
+    
+    
+    public func horizontalScroller(scroller: UIScrollView, imageViewIndex: Int) -> UIView {
+        //自定义AlbumView
+        let coverUrl = albums[imageViewIndex].coverUrl
+//        let view = AlbumView(frame: CGRectMake(0, 0, 200, 200), ablumCover: coverUrl)
+        let view = AlbumView(frame: CGRectMake(0, 0, 200, 200),ablumCover:coverUrl)
+        return view
+    }
+}
+extension AlbumsListViewController:HorizontalScrollerDelegate{
+    
+    public func onclickPageImageView(imageView: UIView) {
+        //点击事件
+    }
+}
---
+++ SISpeciesNotes/Controller/OnclickLikeViewController.swift
@@ -0,0 +1,81 @@
+//
+//  OnclickLikeViewController.swift
+//  SISpeciesNotes
+//
+//  Created by huoshuguang on 16/4/15.
+//  Copyright © 2016年 布衣男儿. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+public class OnclickLikeViewController: UIViewController
+{
+    //签到按钮switch
+    let qdSwitch = UISwitch()
+    //签到计数
+    let count = UILabel()
+    
+    //测试按钮：expectationForPredicate
+    let button = UIButton()
+    
+    public override func viewDidLoad() {
+        //加载UI
+        addViewInSuperView()
+    }
+    
+    
+    //添加点赞按钮，点赞次数,并布局
+    func addViewInSuperView()
+    {
+        //签到Label
+        let qdLabel = UILabel()
+        qdLabel.text = "签到:"
+        view.addSubview(qdLabel)
+        
+        qdSwitch.on = false
+        view.addSubview(qdSwitch)
+        
+        //签到次数
+        let countLabel = UILabel()
+        countLabel.text = "签到次数:"
+        view.addSubview(countLabel)
+        
+        count.text = ""
+        view.addSubview(count)
+        
+        //按钮
+        button.setBackgroundImage(UIImage(named: "IconFlora"), forState: .Normal)
+        button.sizeToFit()
+        view.addSubview(button)
+        button.snp_makeConstraints { (make) in
+            //距顶50，水平居中
+            make.top.equalTo(self.snp_topLayoutGuideBottom).offset(50)
+            make.centerX.equalTo(view)
+        }
+        //
+        qdLabel.snp_makeConstraints { (make) in
+            //距顶部50，水平居中
+            make.top.equalTo(self.snp_topLayoutGuideBottom).offset(100)
+            make.center.equalTo(view).offset(CGPointMake(-20, 0))
+        }
+        
+        qdSwitch.snp_makeConstraints { (make) in
+            //位于qdLabel右边且中心对齐，间距20
+            make.left.equalTo(qdLabel.snp_right).offset(20)
+            make.centerY.equalTo(qdLabel)
+        }
+        
+        countLabel.snp_makeConstraints { (make) in
+            //位于qdSwitch底部间距100，水平居中
+            make.top.equalTo(qdSwitch).offset(100)
+            make.centerX.equalTo(view)
+        }
+        
+        count.snp_makeConstraints { (make) in
+            //位于countLabel底部，间距20，水平居中
+            make.top.equalTo(countLabel).offset(20)
+            make.centerX.equalTo(countLabel)
+        }
+    }
+}
