0f5f75bdb993077dc2889329fa8b50fac9b68257
pengyucheng <pengyucheng@huoshug.local>
2016-11-17 14:49:02 +0800

    学习完成presentationsDemo

--- PresentationsDemo/PresentationsDemo/AlertViewController.swift
+++ PresentationsDemo/PresentationsDemo/AlertViewController.swift
@@ -12,10 +12,27 @@ class AlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func showAlertWasTapped(_ sender: UIButton) {
+    //注意：在iPad下ActionSheet将以popover的形式展现出来，而且当Action的Style为cancel的时候，Action按钮是不会被显示在iPad上的。
+    //ActionSheet的显示效果:需要如下修正：
+    //    alertController.popoverPresentationController?.sourceView = view
+    //    alertController.popoverPresentationController?.sourceRect = sender.frame
+    //原因：在iPad上，ActionSheet会被以popover的形式显示出来，它衣服在当前页面的某一个组件上，因为必须指定一个sourceView用于指定ActionSheet的依附点（在这个空间的周围被弹出），同时还应指定一个sourceRect用于指定他被包含在哪一片区域内
+    
+    @IBAction func showAlertWasTapped(_ sender: UIButton)
+    {
+        let alertController = UIAlertController(title: "一点都简单", message: "来自一点都简单的Alert", preferredStyle: .alert)
+        let delAction = UIAlertAction.init(title: "删除", style: .cancel, handler:{del in
+            print("4444:\(del.title!)")
+        })
+        alertController.addAction(delAction)
+        let okAction = UIAlertAction.init(title: "确定", style: .destructive, handler:
+            {ok in
+                print("OK : \(ok.title!)")
+        })
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: {print("显示成功")})
     }
 }
--- PresentationsDemo/PresentationsDemo/PopoverViewController.swift
+++ PresentationsDemo/PresentationsDemo/PopoverViewController.swift
@@ -16,6 +16,43 @@ class PopoverViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func actionWasTapped(_ sender: UIBarButtonItem) {
+    @IBAction func actionWasTapped(_ sender: UIBarButtonItem)
+    {
+        let storyboard:UIStoryboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "PopoverViewController")
+        vc.modalPresentationStyle = .popover
+        if vc.modalPresentationStyle == .popover {
+            let popover:UIPopoverPresentationController = vc.popoverPresentationController!
+            //barButtonItem决定这个popover页面依附在哪一个按钮下
+            popover.barButtonItem = sender
+            popover.delegate = self
+        }
+
+        present(vc, animated: true, completion: {
+            print("popoverViewController completion")
+        })
+    }
+}
+
+//在iPhone下你没有办法去关闭当前这个弹出来的窗口，为了实现这个功能，我们可以设置一个navigation controller到这个页面。
+extension PopoverViewController:UIPopoverPresentationControllerDelegate
+{
+    //在你 compact-with 的情况下被调用，也就是在这个例子里这个方法只会在iPhone应用里执行这个方法。运行app将得到如下效果。
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .fullScreen
+        //如果你想让iPhone也能够以popver的形式显示这个页面
+//        return .none
+    }
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        //在present页面添加导航控制器
+        let navigationController = UINavigationController.init(rootViewController: controller.presentedViewController)
+        //设置了一个navagationbar并在他的右边添加了一个Done按钮，这个按钮绑定了一个dissmiss方法用来关闭整个窗口。
+        let barDone = UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(PopoverViewController.dismiss as (PopoverViewController) -> () -> ()))
+        navigationController.topViewController?.navigationItem.rightBarButtonItem = barDone
+        return navigationController
+    }
+
+    func dismiss() {
+        dismiss(animated: true, completion: nil)
     }
 }
