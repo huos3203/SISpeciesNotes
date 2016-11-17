f202a5d91ea3b334f525b3029b7609a97e8110ea
pengyucheng <pengyucheng@huoshug.local>
2016-11-17 12:32:59 +0800

    iOS弹出页神器Presentation Controllershttp://www.jianshu.com/p/8740a7ddc99d

---
+++ PresentationsDemo/PresentationsDemo/PopoverViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PopoverViewController.swift
+//  PresentationsDemo
+//
+//  Created by Joyce Echessa on 4/6/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class PopoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func actionWasTapped(sender: UIBarButtonItem) {
+    }
+}
