37480c6bdff943a545176a08c04d2a9802ca6fa9
pengyucheng <pengyucheng@huoshug.local>
2016-11-22 14:22:26 +0800

    /*MARK: 创建复合动画组          注：视图动画无法作为的联动动画中第二动画，即以下代码中视图动画永远无法被执行：                  theAnim.start()                  theViewAnim.start(when: theAnim, reachesProgress: 0.3)          但是，视图动画可以作为联动动画中的导火索动画，触发NSAnimation动画的开始：                 即：只能作为导索动画，来创建复合动画                  theViewAnim.start()                  theAnim.start(when: theViewAnim, reachesProgress: 0.5)          还有，NSAnimation子类重载动画的计算属性currentProgress时：无法作为导索动画，即无法触发第二动画的开始。                 即:只能作为被导索的动画，来创建复合动画                 theOtherAnim.start(when: theViewAnim, reachesProgress: 0.5)          */

--- TextKitNotepad/CustomAnimation.swift
+++ TextKitNotepad/CustomAnimation.swift
@@ -30,7 +30,7 @@ class CustomAnimation:NSViewController,NSAnimationDelegate
     {
         print("创建一个动画对象...")
         //NSAnimation:触发播放音乐
-        theAnim = NSAnimation.init(duration: 30.0,
+        theAnim = NSAnimation.init(duration: 10.0,
                                    animationCurve: .easeInOut)
         theAnim.frameRate = 200.0
         //用blocking模式：nonblockingThreaded 动画在主线程里以循环方式接受用户输入
@@ -38,16 +38,13 @@ class CustomAnimation:NSViewController,NSAnimationDelegate
         theAnim.delegate = self
         
         //SmoothAnimation:执行窗口从屏幕左->右平滑飘过的动画效果
-        theOtherAnim = SmoothAnimation.init(duration: 30.0,
+        theOtherAnim = SmoothAnimation.init(duration: 20.0,
                                             animationCurve: .easeIn)
         theOtherAnim.delegate = self
         theOtherAnim.animationBlockingMode = .nonblockingThreaded //子线程动画
         
         //添加NSViewAnimation
         addViewAnimation()
-        //创建复合动画组
-        theOtherAnim.start(when: theAnim, reachesProgress: 0.5)
-        theViewAnim.start(when: theAnim, reachesProgress: 0.7)
     }
     /*
      awakeFromNib两次被调用：
@@ -62,6 +59,7 @@ class CustomAnimation:NSViewController,NSAnimationDelegate
     override func viewDidAppear()
     {
         //
+        print("======viewDidAppear======")
         let count = 20
         let progMarks:[NSAnimationProgress] = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
         for i in 0..<count {
@@ -71,46 +69,39 @@ class CustomAnimation:NSViewController,NSAnimationDelegate
             theAnim.addProgressMark(progMarks[i])
             //重载currentProgress之后的动画进度无效
             theOtherAnim.addProgressMark(progMarks[i])
+            //想通过animation(animation:didReachProgressMark:)执行其他相关动画进度的操作，必须确保有该步骤，否则进度代理不会执行
+            theViewAnim.addProgressMark(progMarks[i])
         }
     }
     
     @IBAction func ibaStartAnim(_ sender: Any)
     {
         print("启动复合动画：\(theAnim.currentProgress)")
-        theOtherAnim.start()
+        //
+      /*MARK: 创建复合动画组
+         注：视图动画无法作为的联动动画中第二动画，即以下代码中视图动画永远无法被执行：
+                 theAnim.start()
+                 theViewAnim.start(when: theAnim, reachesProgress: 0.3)
+         但是，视图动画可以作为联动动画中的导火索动画，触发NSAnimation动画的开始：
+                即：只能作为导索动画，来创建复合动画
+                 theViewAnim.start()
+                 theAnim.start(when: theViewAnim, reachesProgress: 0.5)
+         还有，NSAnimation子类重载动画的计算属性currentProgress时：无法作为导索动画，即无法触发第二动画的开始。
+                即:只能作为被导索的动画，来创建复合动画
+                theOtherAnim.start(when: theViewAnim, reachesProgress: 0.5)
+         */
+        theViewAnim.start()
+        theAnim.start(when: theViewAnim, reachesProgress: 0.5)
+        theOtherAnim.start(when: theViewAnim, reachesProgress: 0.5)
     }
 
     @IBAction func ibaStopAnim(_ sender: Any)
     {
         theAnim.stop()
     }
-    
     /*
-        addProgressMark(1.0)方法：当mar有变动就会触发通知协议，
-         以下两种情况列外：
-          1. 当SmoothAnimation重写了currentProgress计算属性，该协议会失效不被执行
-          2. 创建复合动画组start(when: reachesProgress:)其中when:参数，即第一动画不能是重载动画的计算属性currentProgress，否则无法启动第二个动画。
-     */
-    func animation(_ animation: NSAnimation, didReachProgressMark progress: NSAnimationProgress)
-    {
-        if animation == theAnim
-        {
-            //progress是耗损弹药的量，可据此更新UI状态进度等
-            print("播放音乐动画：\(progress)")
-            NSSound.init(named: "ddd.mp3")?.play()
-            
-        }
-        else if animation == theOtherAnim
-        {
-            //移动动画
-            print("屏幕上移动动画1111：\(progress)")
-        }
-    }
-    
-    /*
-        MARK: - NSViewAnimation动画
-        使用连动动画开启动画：theViewAnim.start(when: theAnim, reachesProgress: 0.7)
-     */
+        MARK: - NSViewAnimation动画，
+      */
     func addViewAnimation()
     {
         let firstViewFrame = firstView.frame
@@ -135,9 +126,35 @@ class CustomAnimation:NSViewController,NSAnimationDelegate
         ]
         
         theViewAnim = NSViewAnimation.init(viewAnimations: [firstViewDict,secondViewDict])
-        theViewAnim.duration = 1.5
+        theViewAnim.duration = 10
+//        theViewAnim.animationBlockingMode = .nonblockingThreaded
         theViewAnim.animationCurve = .easeIn
+        theViewAnim.delegate = self
 //                theViewAnim.start()
+    }
+    
+    /*MARK: 动画代理
+     addProgressMark(1.0)方法：当mar有变动就会触发通知协议，
+     当NSAnimation子类 SmoothAnimation重写了currentProgress计算属性，该协议会失效不被执行
+     */
+    func animation(_ animation: NSAnimation, didReachProgressMark progress: NSAnimationProgress)
+    {
+        if animation == theAnim
+        {
+            //progress是耗损弹药的量，可据此更新UI状态进度等
+            print("播放音乐动画：\(progress)")
+            NSSound.init(named: "ddd.mp3")?.play()
+            
+        }
+        else if animation == theOtherAnim
+        {
+            //移动动画
+            print("屏幕上移动动画1111：\(progress)")
+        }
+        else if animation == theViewAnim
+        {
+            print("视图动画进度：\(progress)")
+        }
     }
 
 }
